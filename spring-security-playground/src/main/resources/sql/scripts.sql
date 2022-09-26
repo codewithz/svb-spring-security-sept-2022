03f62377913d946e9c389b817e59e9aab8228d0f
Zartab Nakhwa <zartab@codewithz.com>
2022-09-26 13:14:51 +0530

    Password Encoding : BCrypt Password

--- spring-security-playground/src/main/resources/sql/scripts.sql
+++ spring-security-playground/src/main/resources/sql/scripts.sql
@@ -35,6 +35,22 @@ CREATE TABLE `customer` (
   
   INSERT INTO `customer` (`email`, `pwd`, `role`)
  VALUES ('john.doe@example.com', '54321', 'admin');
+ 
+ drop table customer;
+ 
+ CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `pwd` varchar(200) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `customer` (`email`, `pwd`, `role`)
+ VALUES ('john.doe@example.com', '$2a$12$N9OkG7sYh2Jgwheoc9USw.j6PpTtGF42ufyfANfTJ1UBkq/iiLTGu', 'admin');
+ 
+ 
+ https://bcrypt-generator.com/
 
   
   
