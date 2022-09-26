abff6c8f279c7b18e74f03d41cbe0e33930135bb
Zartab Nakhwa <zartab@codewithz.com>
2022-09-26 15:40:44 +0530

    Authn and AUthz: SQL Changes

--- spring-security-playground/src/main/resources/sql/scripts.sql
+++ spring-security-playground/src/main/resources/sql/scripts.sql
@@ -50,7 +50,51 @@ INSERT INTO `customer` (`email`, `pwd`, `role`)
  VALUES ('john.doe@example.com', '$2a$12$N9OkG7sYh2Jgwheoc9USw.j6PpTtGF42ufyfANfTJ1UBkq/iiLTGu', 'admin');
  
  
+
  https://bcrypt-generator.com/
+ 
+ -- CHanges for Authentication and Authorization -----
+ 
+ Drop table customer;
+
+CREATE TABLE `customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile_number` varchar(20) NOT NULL,
+  `pwd` varchar(500) NOT NULL,
+  `role` varchar(100) NOT NULL,
+  `create_dt` date DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+);
+
+INSERT INTO `customer` (`name`,`email`,`mobile_number`, `pwd`, `role`,`create_dt`)
+ VALUES ('Tom','tom@example.com','9876548337', '$2a$12$IjYMmyZ3/cPo3MeKt8ZdMOeKk8Qz0TRE9gcTWofXkAXBMbFTgQxVm', 'admin',CURDATE());
+
+
+INSERT INTO `customer` (`name`,`email`,`mobile_number`, `pwd`, `role`,`create_dt`)
+ VALUES ('Alex','alex@example.com','9876548337', '$2a$12$VQnUCqzQrEcDLFydVBuiyOFUnvG8Ki7eezX3UUS9aXiWCA1P5B5G2', 'admin',CURDATE());
+
+
+INSERT INTO `customer` (`name`,`email`,`mobile_number`, `pwd`, `role`,`create_dt`)
+ VALUES ('Mike','mike@example.com','9876548337', '$2a$12$fi9lklOvkTitvJpQ8HJSSeXu/PEhiF.iFXBzeY4Npi/JMfu1n8w5e', 'admin',CURDATE());
+
+drop table authorities;
+
+CREATE TABLE `authorities` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
+);
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'READ');
+ 
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'WRITE');
 
   
   
