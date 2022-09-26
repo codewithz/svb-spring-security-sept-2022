3c41f9272ad1ebe68a95766763578742bff0986a
Zartab Nakhwa <zartab@codewithz.com>
2022-09-26 17:23:10 +0530

    JWT : JWT Generator Filter

--- spring-security-playground/src/main/java/com/svb/constants/SecurityConstants.java
+++ spring-security-playground/src/main/java/com/svb/constants/SecurityConstants.java
@@ -2,6 +2,6 @@ package com.svb.constants;
 
 public class SecurityConstants {
 
-	public static final String KEY="abcde12345xyzmno";
+	public static final String JWT_KEY="abcde12345xyzmno";
 	public static final String JWT_HEADER="Authorization";
 }
