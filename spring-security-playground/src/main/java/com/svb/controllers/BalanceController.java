1fbe8e94e2d7d5db6e18c989eda3badf794ab001
Zartab Nakhwa <zartab@codewithz.com>
2022-09-26 10:17:20 +0530

    Added Controllers

---
+++ spring-security-playground/src/main/java/com/svb/controllers/BalanceController.java
@@ -0,0 +1,17 @@
+package com.svb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+	
+	@GetMapping("/myBalance")
+	public String getBalanceDetails(String input) {
+		return "Here are the balance details from the DB";
+	}
+
+}
